32e79b381813d03c7dabe13fd4421c615b6365a9
LizaBrnk <liza.brnk@gmail.com>
2023-02-21 19:48:41 +0200

    main class

--- Lab_2/src/Vector1.java
+++ Lab_2/src/Vector1.java
@@ -3,8 +3,8 @@ public class Vector1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
             int choice = 0;
-            Vector v1 = new Vector();
-            Vector v2 = new Vector();
+            Vector2 v1 = new Vector2();
+            Vector2 v2 = new Vector2();
 
         System.out.print("Input x, y and z coordinates for Vector 1: ");
             v1.X = in.nextDouble();
@@ -29,21 +29,21 @@ public class Vector1 {
                     System.out.println("V2 module: " + v2.GetModule());
                     break;
                 case 2:
-                    Vector sum = v1.Sum(v2);
+                    Vector2 sum = v1.Sum(v2);
                     System.out.println("Sum of v1 and v2: " + sum.X + ", " + sum.Y + ", " + sum.Z);
                     break;
                 case 3:
                     System.out.println("Scalar product: " + v1.ScalarProduct(v2));
                     break;
                 case 4:
-                    Vector v3 = v1.VectorProduct(v2);
+                    Vector2 v3 = v1.VectorProduct(v2);
                     System.out.println("Vector product: " + v3.X + ", " + v3.Y + ", " + v3.Z);
                     break;
                 case 5:
                     System.out.print("Input number to multiply vector: ");
                     double num = in.nextDouble();
-                    Vector mv1 = v1.MultiplyByNumber(num);
-                    Vector mv2 = v2.MultiplyByNumber(num);
+                    Vector2 mv1 = v1.MultiplyByNumber(num);
+                    Vector2 mv2 = v2.MultiplyByNumber(num);
                     System.out.println("V1: " + mv1.X + ", " + mv1.Y + ", " + mv1.Z);
                     System.out.println("V2: " + mv2.X + ", " + mv2.Y + ", " + mv2.Z);
                     break;
@@ -54,18 +54,18 @@ public class Vector1 {
     }
 }
 
-class Vector {
+class Vector2 {
     double X;
     double Y;
     double Z;
 
-    Vector() {
+    Vector2() {
         X = 0;
         Y = 0;
         Z = 0;
     }
 
-    Vector(double x, double y, double z) {
+    Vector2(double x, double y, double z) {
         X = x;
         Y = y;
         Z = z;
@@ -75,19 +75,19 @@ class Vector {
         return Math.pow(X * X + Y * Y + Z * Z, 0.5);
     }
 
-    Vector Sum(Vector v) {
-        return new Vector(X + v.X, Y + v.Y, Z + v.Z);
+    Vector2 Sum(Vector2 v) {
+        return new Vector2(X + v.X, Y + v.Y, Z + v.Z);
     }
 
-    double ScalarProduct(Vector v) {
+    double ScalarProduct(Vector2 v) {
         return X * v.X + Y * v.Y + Z * v.Z;
     }
 
-    Vector VectorProduct(Vector v) {
-        return new Vector(Y * v.Z - Z * v.Y, Z * v.X - X * v.Z, X * v.Y - Y * v.X);
+    Vector2 VectorProduct(Vector2 v) {
+        return new Vector2(Y * v.Z - Z * v.Y, Z * v.X - X * v.Z, X * v.Y - Y * v.X);
     }
 
-    Vector MultiplyByNumber(double a) {
-        return new Vector(X * a, Y * a, Z * a);
+    Vector2 MultiplyByNumber(double a) {
+        return new Vector2(X * a, Y * a, Z * a);
     }
 }
--- Lab_3/src/Main.java
+++ Lab_3/src/Main.java
@@ -1,5 +1,164 @@
+import java.util.Scanner;
+
+interface Run {
+    String GetName();
+    double GetCurrentSpeed();
+    int GetWeight();
+    int GetPassengers();
+    int GetWheels();
+}
+abstract class Machine implements Run{
+    final String name;
+    protected double Speed;
+    final static double MAX_SPEED = 300;
+    final static double MIN_SPEED = 1;
+    public static int MIN_PASSAMOUNT = 0;
+    public static int MAX_PASSAMOUNT = 4;
+    Scanner s = new Scanner(System.in);
+
+    Machine() {
+        System.out.print("\n Введіть назву машини: ");
+        while(!s.hasNextLine())
+        {
+            System.out.print("Введіть коректне значення: ");
+            s.next();
+        }
+        this.name = s.nextLine();
+
+        System.out.print("\n Введіть швидкість машини: ");
+        while(!s.hasNextDouble())
+        {
+            System.out.print("Введіть коректне значення: ");
+            s.next();
+        }
+        double speed = s.nextDouble();
+
+        if (speed <= MAX_SPEED && speed >= MIN_SPEED){
+            this.Speed = speed;
+        } else {
+            this.Speed = 0;
+        }
+    }
+    public String GetName() {
+        return this.name;
+    }
+}
+class Auto extends Machine {
+    int weight;
+    int passengers;
+    int wheels;
+
+    Auto(){
+        super();
+        System.out.print("\n Введіть вагу машини: ");
+        while(!s.hasNextInt())
+        {
+            System.out.print("Введіть коректне значення: ");
+            s.next();
+        }
+        this.weight = s.nextInt();
+
+        System.out.print("\n Введіть кількість пасажирів: ");
+        while(!s.hasNextInt())
+        {
+            System.out.print("Введіть коректне значення: ");
+            s.next();
+        }
+        int Passengers = s.nextInt();
+        if (Passengers >= MIN_PASSAMOUNT && Passengers<=MAX_PASSAMOUNT){
+            this.passengers = Passengers;
+        } else {
+            this.passengers = 0;
+        }
+
+        System.out.print("\n Введіть кількість коліс машини: ");
+        while(!s.hasNextInt())
+        {
+            System.out.print("Введіть коректне значення: ");
+            s.next();
+        }
+        this.wheels = s.nextInt();
+    }
+
+    public double GetCurrentSpeed(){
+        return this.Speed;
+    }
+    public int GetWeight(){
+        return this.weight;
+    }
+    public int GetPassengers(){
+        return this.passengers;
+    }
+    public int GetWheels(){
+        return this.wheels;
+    }
+    void Information() {
+        System.out.println("ІМ'Я: " + name + "  ШВИДКІСТЬ: " + Speed + "  ВАГА: " + weight +
+                "  КІЛЬКІСТЬ ПАСАЖИРІВ: " + passengers  + "  КІЛЬКІСТЬ КОЛІС: " + wheels);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Auto a = null;
+        Scanner in = new Scanner(System.in);
+        int choice = 0;
+        while (choice!=12) {
+            System.out.println("\n 1. Створити об'єкт");
+            System.out.println(" 2. Вивести дані про об'єкт");
+            System.out.println(" 3. Вивести ім'я об'єкту");
+            System.out.println(" 4. Вивести швидкість об'єкту");
+            System.out.println(" 5. Вивести вагу об'єкту");
+            System.out.println(" 6. Вивести кількість пасажирів об'єкту");
+            System.out.println(" 7. Вивести кількість коліс об'єкту");
+            System.out.println(" 0. Вийти");
+            System.out.println("Виберіть завдання:");
+            choice = in.nextInt();
+            switch (choice) {
+                case 1:
+                    a = new Auto();
+                    break;
+                case 2:
+                    if (a!=null)
+                        a.Information();
+                    else
+                        System.out.println("Об'єкт ще не створено");
+                    break;
+                case 3:
+                    if (a!=null)
+                        System.out.println("ІМ'Я: " + a.GetName());
+                    else
+                        System.out.println("Об'єкт ще не створено");
+                    break;
+                case 4:
+                    if (a!=null)
+                        System.out.println("ШВИДКІСТЬ: " + a.GetCurrentSpeed());
+                    else
+                        System.out.println("Об'єкт ще не створено");
+                    break;
+                case 5:
+                    if (a!=null)
+                        System.out.println("ВАГА: " + a.GetWeight());
+                    else
+                        System.out.println("Об'єкт ще не створено");
+                    break;
+                case 6:
+                    if (a!=null)
+                        System.out.println("КІЛЬКІСТЬ ПАСАЖИРІВ: " + a.GetPassengers());
+                    else
+                        System.out.println("Об'єкт ще не створено");
+                    break;
+                case 7:
+                    if (a!=null)
+                        System.out.println("КІЛЬКІСТЬ КОЛІС: " + a.GetWheels());
+                    else
+                        System.out.println("Об'єкт ще не створено");
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Некоректний ввід");
+            }
+        }
     }
 }
